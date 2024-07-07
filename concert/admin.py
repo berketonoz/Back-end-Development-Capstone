1464c3e1f08e78d5e0127a079a81065e71245823
berketonoz <btonoz@sabanciuniv.edu>
2024-07-07 23:09:30 +0300

    admin site register update

--- concert/admin.py
+++ concert/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Concert
+from .models import Concert, Song, Photo
 
 admin.site.register(Concert)
+admin.site.register(Photo)
+admin.site.register(Song)
